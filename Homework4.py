180f5ff666bf0e895905e052b06cd7dcf193e49e
don-bigdad <bogdan.maksimenko.2002@gmail.com>
2022-01-21 01:42:58 +0200

    firs stem of four homework

--- Homework4.py
+++ Homework4.py
@@ -1,3 +1,30 @@
-print("Hello world")
-x=5
-print(x)
+# счастливый билет.проверить является ли шестизначный билет счастливым.Если получится не привязыватся к
+# номеру билета выполнить код
+# 6-ти значный билет
+g = list(map(int, str(input("enter your ticket"))))
+if g[0] + g[1] + g[2] == g[3] + g[4] + g[5]:
+    print("your ticket is lucky")
+else:
+    print("You have a regular ticket")
+# Любой билет
+f = list(map(int, str(input("enter your ticket"))))
+h = len(f) // 2
+if sum(f[0:h]) == sum(f[h:]):
+    print("your ticket is lucky")
+else:
+    print("You have a regular ticket")
+# число палиндром.Для начала разберемся с 6-ти значным числом
+n = list(map(int, str(input("enter a number"))))
+j = n[::-1]
+if n[0:3] == j[0:3]:
+    print("число не полиндром")
+else:
+    print("вы ввели простое число")
+# программа,которая работает с любым числом.Другим способом.
+n = list(map(int, str(input("enter a number"))))
+u = len(n) // 2
+k = n[::-1]
+if n[0:u] == k[0:u]:
+    print("Число является полиндромом", n)
+else:
+    print("число не полиндром", n)
