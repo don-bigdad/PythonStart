b90668268baa9f07a116c8f54479f4e38442cdd2
Bogdan <97698639+don-bigdad@users.noreply.github.com>
2022-01-25 12:23:56 +0200

    Update Homework4.py

--- Homework4.py
+++ Homework4.py
@@ -1,27 +1,27 @@
 # счастливый билет.проверить является ли шестизначный билет счастливым.Если получится не привязыватся к
 # номеру билета выполнить код
 # 6-ти значный билет
-g = list(map(int, str(input("enter your ticket"))))
+g = list(map(int,input("enter your ticket")))
 if g[0] + g[1] + g[2] == g[3] + g[4] + g[5]:
     print("your ticket is lucky")
 else:
     print("You have a regular ticket")
 # Любой билет
-f = list(map(int, str(input("enter your ticket"))))
+f = list(map(int,input("enter your ticket")))
 h = len(f) // 2
 if sum(f[0:h]) == sum(f[h:]):
     print("your ticket is lucky")
 else:
     print("You have a regular ticket")
 # число палиндром.Для начала разберемся с 6-ти значным числом
-n = list(map(int, str(input("enter a number"))))
+n = list(map(int,input("enter a number")))
 j = n[::-1]
 if n[0:3] == j[0:3]:
     print("число не полиндром")
 else:
     print("вы ввели простое число")
 # программа,которая работает с любым числом.Другим способом.
-n = list(map(int, str(input("enter a number"))))
+n = list(map(int,input("enter a number")))
 u = len(n) // 2
 k = n[::-1]
 if n[0:u] == k[0:u]:
