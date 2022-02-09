b46e8571470e0728c55b572f1827e76ec8229132
don-bigdad <bogdan.maksimenko.2002@gmail.com>
2022-02-09 15:49:47 +0200

    1st exercise done

--- Homework9.py
+++ Homework9.py
@@ -1,1 +1,13 @@
-print("some text")
+# Напишите программу, которая сгенерирует два списка. Один с числами
+# кратными 3, другой с числами кратными 5. С помощью множеств
+# создайте список с числами, которые есть в обоих множествах
+list1=[]
+for element in range(3,100,3):
+    list1.append(element)
+set_1=set(list1)
+list2=[]
+for element in range(5,100,5):
+    list2.append(element)
+set_2=set(list2)
+set_3=set_1.intersection(set_2)
+print(set_3)
